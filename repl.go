cc1fb2ed0ddef5e6bad9311d768cd857d1019096
JWhite36929 <jamestwhite361@gmail.com>
2026-01-21 13:16:15 -0500

    initial repl

--- repl.go
+++ repl.go
@@ -2,7 +2,37 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+		
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
+
 
 func cleanInput(text string) []string {
 	//split user input into "words" based on whitespace 
@@ -14,4 +44,25 @@ func cleanInput(text string) []string {
 	words := strings.Fields(lowered)
 	return words
 	
+}
+
+type cliCommand struct {
+	name string 
+	description string
+	callback func()error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+	}
 }
