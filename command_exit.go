d25c8373ad3b78b93168e943e1b258ae26064e1a
JWhite36929 <jamestwhite361@gmail.com>
2026-01-28 11:05:27 -0500

    save data w/ json

--- command_exit.go
+++ command_exit.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/jwhite36929/pokedexcli/internal/pokeapi"
 )
 
+type configSave struct {
+	PokeapiClient pokeapi.Client             `json:"pokeApiClient"`
+	CaughtPokemon map[string]pokeapi.Pokemon `json:"caughtPokemon"`
+}
+
 func commandExit(cfg *config, args ...string) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
+	fmt.Println("Saving data...")
+
+	saveData := configSave{
+		PokeapiClient: cfg.pokeapiClient,
+		CaughtPokemon: cfg.caughtPokemon,
+	}
+
+	jsonData, err := json.MarshalIndent(saveData, "", " ")
+	if err != nil {
+		log.Fatal("Error marshalling to JSON:", err)
+	}
+
+	saveFile := "pokedexSave.json"
+	err = os.WriteFile(saveFile, jsonData, 0644)
+	if err != nil {
+		log.Fatal("Error writing to file: ", err)
+	}
+
+	fmt.Println("Data saved successfully, Goodbye!")
 	os.Exit(0)
 	return nil
 }
